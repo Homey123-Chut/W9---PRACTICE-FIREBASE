62a8207d4763495030bc84be5e423d58b3484f74
Homey123-Chut <homey.chut@student.cadt.edu.kh>
2026-03-22 16:17:33 +0700

    W9-02 – Inject the artist repository to the main_dev.dart

--- main_dev.dart
+++ main_dev.dart
@@ -1,4 +1,5 @@
 import 'package:provider/provider.dart';
+import 'package:w9/data/repositories/artists/artist_repository.dart';
  
 import 'data/repositories/songs/song_repository_firebase.dart';
 import 'main_common.dart';
@@ -6,6 +7,8 @@ import 'data/repositories/settings/app_settings_repository_mock.dart';
 import 'data/repositories/songs/song_repository.dart';
 import 'ui/states/player_state.dart';
 import 'ui/states/settings_state.dart';
+import './data/repositories/artists/artist_repository.dart';
+import './data/repositories/artists/artist_repository_firebase.dart';
 
 /// Configure provider dependencies for dev environment
 List<InheritedProvider> get devProviders {
@@ -23,6 +26,9 @@ List<InheritedProvider> get devProviders {
     ChangeNotifierProvider<AppSettingsState>(
       create: (_) => AppSettingsState(repository: appSettingsRepository),
     ),
+
+    // 4 - Inject the artist repository
+    Provider<ArtistRepository>(create: (_) => ArtistRepositoryFirebase()),
   ];
 }
 
